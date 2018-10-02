fe608e553acc4f57dc10c0618bae1173d731ef61
Otto André Haaland <oahaal03@student.uia.no>
2018-10-02 02:11:46 +0200

    added userroutes to server.js

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const userRoutes = require('./api/routes/users');
+
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.listen(app.get('port'), function(){
     console.log('Your server is now running at port: ' + app.get('port'));
 });
+
+app.use('/api/user',userRoutes);
