59e3f8eb48d3e1c0e4b550ccfd2127e4bde58913
Otto André Haaland <oahaal03@student.uia.no>
2018-10-02 10:28:56 +0200

    Element routes updated

--- api/routes/elements.js
+++ api/routes/elements.js
@@ -1,42 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const users = [
-    {id:1,name:"Fluffy",email:"fluffy@uia.no",password:"123456"},
-    {id:2,name:"Flappy",email:"flappy@uia.no",password:"123456"},
-    {id:3,name:"Floppy",email:"floppy@uia.no",password:"123456"}
+const elements = [
+    {id:1,heading:"Fluffystuff",body:"My list element",owner:"1",list:"1"},
+    {id:2,heading:"Flappystuff",body:"My list element",owner:"2",list:"2"},
+    {id:3,heading:"Flippystuff",body:"My list element",owner:"3",list:"3"}
 ]
 
 router.get('/', (req,res) => {
-    res.status(200).json(users).end();
+    res.status(200).json(elements).end();
 });
 
 router.post('/create',(req,res) =>{
-    if(validateUserCreationInput() === true){
-        res.status(201).json({msg:"User created"}).end();
+    if(true === true){
+        res.status(201).json({msg:"Element created"}).end();
     } else{
-        res.status(400).json({msg:"User creation failed"}).end();
+        res.status(400).json({msg:"Element creation failed"}).end();
     }
-})
-
-router.post('/auth',(req,res) =>{
-    if(validateUserAuthInput() === true){
-        res.status(200).json({msg:"User logged in"}).end();
-    } else{
-        res.status(401).json({msg:"Unauthorized user"}).end();
-    }
-})
-
-
+});
 
 module.exports = router;
-
-function validateUserAuthInput(){
-    //todo..validate user input
-    return true;
-}
-
-function validateUserCreationInput(){
-    //todo..validate user input
-    return true;
-}
