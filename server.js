f3773c835799f04b2fdc5756522240fe5553611a
Otto <otto.andre@deep-north.com>
2018-10-14 21:44:02 +0200

    express server running

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+const app = new express();
 
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static('public'));
 app.use(bodyParser.json());
+
 app.listen(app.get('port'), function(){
     console.log('Your server is now running at port: ' + app.get('port'));
 });
