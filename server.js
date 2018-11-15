f94dfd9f5e28db55cedd72b161e9ef12bf733ff1
Otto <otto.andre@deep-north.com>
2018-11-15 20:48:30 +0100

    added listRoutes to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const userRoutes = require('./controller/routes/api/userRoutes');
+const listRoutes = require('./controller/routes/api/listRoutes');
 const authorizeRoute = require('./controller/routes/authorize');
 
 app.set('port', (process.env.PORT || 8080));
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 
 app.use(authorizeRoute);
 app.use(userRoutes);
+app.use(listRoutes);
 
 app.listen(app.get('port'), function(){
     console.log('Your server is now running at port: ' + app.get('port'));
