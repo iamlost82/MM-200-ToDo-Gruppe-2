44adeec66feb8641276d42036cf957f35ee650eb
Otto <otto.andre@deep-north.com>
2018-11-20 21:35:53 +0100

    rollback due to commit by mistake

--- public/controller/ctr_home.js
+++ public/controller/ctr_home.js
@@ -2,5 +2,9 @@ todoListCtr.ctr_home = function(){
     if(!localStorage.getItem('token')){
         todoListCtr.view_loginUser();
     }
-    
+    document.querySelector('#logOutBtn').addEventListener('click', function(){
+        todoListCtr.loadingSpinner('show');
+        localStorage.clear();
+        todoListCtr.view_loginUser();
+    });    
 }
