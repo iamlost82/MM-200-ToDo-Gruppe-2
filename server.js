18e8bed6a6e00becbb04a9d6d331552ebd9c49c5
Otto André Haaland <oahaal03@student.uia.no>
2018-11-20 15:35:54 +0100

    added cors to server for testing

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const elementRoutes = require('./controller/routes/api/elementRoutes')
 const authorizeRoute = require('./controller/routes/authorize');
 
 app.set('port', (process.env.PORT || 8080));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
